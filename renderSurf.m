dd9e49ed5f3d0865f5e5ddeec758487cec25ec62
Denis Schluppeck <denis.schluppeck@gmail.com>
2015-03-10 17:24:05 +0000

    allow rendering of data

--- renderSurf.m
+++ renderSurf.m
@@ -1,7 +1,7 @@
-function [ h_ ] = renderSurf(s)
+function [ h_ ] = renderSurf(s, data)
 %renderSurf - use patch to render a surface struct
 %
-%      usage: [ h_ ] = renderSurf( s )
+%      usage: [ h_ ] = renderSurf( s, data )
 %         by: ds1
 %       date: Mar 09, 2015
 %     inputs: s
@@ -12,7 +12,16 @@ function [ h_ ] = renderSurf(s)
 %
 %        e.g: 
 %             sr = loadSurfVTK('surf/rh.white.vtk')
-%             firgure, renderSurf( sr )
+%             figure, renderSurf( sr )
+%
+%       -or-
+%             s = loadSurfVTK('surf/rh.sphere.vtk')
+%             c = loadSurfVTK('surf/rh.curv.vtk')
+%             figure, renderSurf( s, sign(c.data) )
+
+if nargin < 2
+    data = [];
+end
 
 if nargin < 1 || ~isstruct(s)
     help renderSurf
@@ -24,12 +33,25 @@ if ~all(isfield(s, {'vtcs', 'tris', 'Nvtcs', 'Ntris', 'Nedges', 'filename'}))
     return
 end
 
+if exist('data') && ~isempty('data') && numel(data) == s.Nvtcs
+    % user wants to render data provides
+    facevertexcdata = data(:);
+elseif (isfield(s, 'data') && ~isempty(s.data)) && not(numel(s.data)==1 && all(s.data == 0))
+   disp('(renderSurf) data present')
+    facevertexcdata = s.data;
+else
+    % simply constant
+    facevertexcdata = ones(size(s.vtcs));
+end
+
 h_ = patch('vertices', s.vtcs, 'faces', s.tris, ...
-    'facevertexcdata', ones(size(s.vtcs)) );
+    'facevertexcdata', facevertexcdata );
+material dull
 shading interp
 colormap gray
 light
 axis equal
 axis vis3d
+axis off
 
 end
