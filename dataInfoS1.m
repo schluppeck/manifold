b3730735dd0ceaa3477f6bdb88ffa37dc5071b6a
Denis Schluppeck <denis.schluppeck@gmail.com>
2015-04-10 21:32:31 +0100

    adds some code showing how to find points on the sphere corresponding to white/midcortex/gray matter points that are in our fMRI data volume.

--- dataInfoS1.m
+++ dataInfoS1.m
@@ -12,7 +12,7 @@
 %        e.g: 
 %             dataInfoS1
 
-% 4d functional imaging data is stored in this mat file
+%% 4d functional imaging data is stored in this mat file
 % we usually store data in a different format (NIFTI), but for convenience,
 % here I saved out the same info into a MAT file
 fname = 'somato-fMRI-periodic';  % big data file. > 60mb
@@ -46,6 +46,8 @@ axis image
 colormap(gray), colorbar
 title(sprintf('Slice through data at z=%i, t=%i', whichZ, whichT))
 
+%% onto the surface
+%
 % how does this map onto the 2d surface?
 % fMRI data were obtained with a stimulus on the left hand 
 % therefore look on right hemisphere
@@ -84,6 +86,8 @@ s1_ = plot3(S1.volumeCoords(1:skip:end,1), ...
     S1.volumeCoords(1:skip:end,2), ...
     S1.volumeCoords(1:skip:end,3), 'b.');
 
+%% onto the sphere
+%
 % now load in sphere representation of the hemisphere
 sSphere = loadSurfVTK('surf/rh.sphere.vtk', 1); 
 
@@ -97,11 +101,88 @@ sCurve = loadSurfVTK('surf/rh.curv.vtk', 1);
 % |patch| to make the code a bit cleaner.
 
 figure, subplot(2,1,1)
-renderSurf(sSphere, sign(sCurve.data))
+renderSurf(sSphere, -sign(sCurve.data))
 title('sign(curvature) rendered on spherical representation')
 
 subplot(2,1,2)
 renderSurf(sCurve)
 title('curvature rendered on normal/white matter representation')
 colormap(gray)
+
+%% to find the points of interest on the sphere 
+%
+% discussion c. 2015-04-10
+%
+% find vertices of interest in e.g. the "white" surface. this corresponds 
+% to the surface at the interface between white matter and gray matter.
+%
+% then, knowing the vertex IDs, we can look up spatial location from the
+% .sphere surfaces for the same cortical hemisphere & subject
+
+% coordinates of voxels in 3d space (homogeneous)
+S1.volumeCoords;
+
+% vertices of the "white matter surface"
+s.vtcs;
+
+% vertices of the "gray matter surface"
+g = loadSurfVTK('surf/rh.pial.vtk', 1); 
+
+% mid cortex is the halfway point between white and gray matter
+% the surfaces have corresponding vertices, so we can use the following
+% trick
+
+m.vtcs = s.vtcs + 0.5 .* (g.vtcs - s.vtcs); % half way between them
+m.tris = s.tris; % triangulation stays the same
+m.Ntris = s.Ntris;
+m.Nvtcs = s.Nvtcs;
+m.filename = 'calculated from wm and gm surfaces';
+
+% there is a c/mex implementation of this, courtesy of wade/wandell
+% (VISTA soft) to speed things up (assignToNearest.m/mexmaci)
+% 
+% but matlab's knnsearch should do the right thing:
+%  first input: point-cloud, second input: query points (making sure not to use homogeneous coords!).
+
+% can try to find out which vertices are closest in WM, mid cortex and GM
+% in our software, something slightly different happens, but this should
+% work.
+idxS = knnsearch(s.vtcs, S1.volumeCoords(:,1:3));
+idxM = knnsearch(m.vtcs, S1.volumeCoords(:,1:3));
+idxG = knnsearch(g.vtcs, S1.volumeCoords(:,1:3));
+
+% some voxels will point to the same 3d point, so could take "unique", but
+% set operation will take care of duplicates, too
+
+idx = union(union(idxS, idxM), idxG);
+
+
+% now render, taking the INDICES of the original surface, but the 3d
+% locations of those vertices mapped into the sphere by the freesurfer
+% program
+
+figure
+renderSurf(sSphere, -sign(sCurve.data));
+
+title('points of interest in 3d')
+
+hold on
+
+% here we take the 3d coords on a sphere (using indices from the closest
+% points in the folded surface)
+spherePoints_ = plot3(sSphere.vtcs(idx,1), ...
+    sSphere.vtcs(idx,2), ...
+    sSphere.vtcs(idx,3), 'b.', 'markersize',5);
+
+axis vis3d
+
+% so the appropriate 3d coords (on the sphere) are just
+sSphere.vtcs(idx,:);
+
+% show some:
+sSphere.vtcs(idx(1:10),:)
+
+% and on to spherical coords via
+% [TH,PHI,R] = cart2sph(X,Y,Z)
+
 
