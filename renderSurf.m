99c825b7e4015d90950e1ad0efe40aa59a5a2ff0
Denis Schluppeck <denis.schluppeck@gmail.com>
2015-05-20 15:05:27 +0100

    adds light switch and adds new behaviour for superimposing data: if numel(data vector) is the same as the surface, then it uses all of those, otherwise try setting the facevertexcdata only at vertices specified in data(:,1) to the values in data(:,2)

--- renderSurf.m
+++ renderSurf.m
@@ -1,10 +1,10 @@
-function [ h_ ] = renderSurf(s, data)
+function [ h_ ] = renderSurf(s, data, lightOn)
 %renderSurf - use patch to render a surface struct
 %
 %      usage: [ h_ ] = renderSurf( s, data )
 %         by: ds1
 %       date: Mar 09, 2015
-%     inputs: s
+%     inputs: s, data, lightOn
 %    outputs: h_
 %
 %    purpose: shortcut for rendering a surface stored in a struct with some
@@ -18,6 +18,10 @@ function [ h_ ] = renderSurf(s, data)
 %             s = loadSurfVTK('surf/rh.sphere.vtk')
 %             c = loadSurfVTK('surf/rh.curv.vtk')
 %             figure, renderSurf( s, sign(c.data) )
+
+if nargin < 3
+    lightOn = true;
+end
 
 if nargin < 2
     data = [];
@@ -33,15 +37,33 @@ if ~all(isfield(s, {'vtcs', 'tris', 'Nvtcs', 'Ntris', 'Nedges', 'filename'}))
     return
 end
 
-if exist('data') && ~isempty('data') && numel(data) == s.Nvtcs
+if exist('data') && ~isempty('data') && numel(data) > 1
     % user wants to render data provides
-    facevertexcdata = data(:);
-elseif (isfield(s, 'data') && ~isempty(s.data)) && not(numel(s.data)==1 && all(s.data == 0))
+    if isvector(data) && numel(data) == s.Nvtcs
+        % data is passed in as a vector the same size as vtcs
+        facevertexcdata = data(:);
+        isDataSubset = false;
+    else
+        % data is passed in with with vertex indices in col 1 
+        try 
+
+          facevertexcdata = zeros(s.Nvtcs,1);
+          facevertexcdata(data(:,1)) = data(:,2);
+          isDataSubset = true;
+          
+        catch
+            disp('data needs to be length(Nvtcs) or [idx(:), data(:)]')
+            return
+        end
+    end
+    elseif (isfield(s, 'data') && ~isempty(s.data)) && not(numel(s.data)==1 && all(s.data == 0))
    disp('(renderSurf) data present')
     facevertexcdata = s.data;
+    isDataSubset = false;
 else
     % simply constant
     facevertexcdata = ones(size(s.vtcs));
+    isDataSubset = false;
 end
 
 if s.Ntris == 0
@@ -60,7 +82,15 @@ else
     material dull
     shading interp
     colormap gray
+    if lightOn
+        light
+    else
+    if isDataSubset 
+        hold on
+        h_ = scatter3(s.vtcs(data(:,1),1), s.vtcs(data(:,1),2), s.vtcs(data(:,1),3), ...
+            10, 'r.'); 
     light
+    end
 end
 axis equal
 axis vis3d
