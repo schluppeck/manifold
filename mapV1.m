dd1e64b274242bde5462e9b954f366d70fa71bdf
David Arnoldussen <david.arnoldussen@nottingham.ac.uk>
2015-08-10 16:32:32 +0100

    adds additional input argument to check whether to display patch

--- mapV1.m
+++ mapV1.m
@@ -1,4 +1,4 @@
-function [ returnData ] = mapV1(subject, hemi, subjectRoot, showSphere, showPlot)
+function [ returnData ] = mapV1(subject, hemi, subjectRoot, showSphere, showPlot, firstIteration)
 %mapV1 - get V1 definition automatically in a subject
 %
 %      usage: [  ] = mapV1(subject, hemi, subjectRoot, showSphere )
@@ -25,11 +25,11 @@ function [ returnData ] = mapV1(subject, hemi, subjectRoot, showSphere, showPlot
 if ieNotDefined('subjectRoot'), subjectRoot = '/data/anatomy/freesurfer/subjects-7T/'; end
 if ieNotDefined('subject'), subject = 'fsaverage'; end
 if ieNotDefined('hemi'), hemi = 'rh'; end
-if ieNotDefined('showSphere'), showSphere = true; end
-if ieNotDefined('showPatchForDebug'), showPatchForDebug = true; end
+if ieNotDefined('showSphere'), showSphere = false; end
+if ieNotDefined('showPatchForDebug'), showPatchForDebug = false; end
 
-if ieNotDefined('showPlot'), showPlot = true; end
-
+if ieNotDefined('showPlot'), showPlot = false; end
+if ieNotDefined('firstIteration'), firstIteration = false; end
 
 % probability threshold for deciding what is INSIDE V1
 if ieNotDefined('thr'), thr = 0.8; end % 0.8
@@ -194,7 +194,7 @@ else
     Wrotated = []; % no need to keep an extra copy. W is still rotated
 end
 
-if showPlot
+if showPlot && ~firstIteration
     if showSphere
         % and make the 2nd patch a subplot figure, too
         subplot(1,3,[2 3])
@@ -214,7 +214,7 @@ end
 % rotation 0 rad!
 [xform_after, hA,pA] = fitV1ellipse(W, showPlot,[],[],Wraw); % either show or don't
 
-if showPlot
+if showPlot && ~firstIteration
     colormap(gray)
     caxis([-5 5])
     daspect([2 1 1]) % axis equal
